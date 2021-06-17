7395009a5ae82cdef1eeff150713970da57b873a
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-06-17 15:55:10 +0600

    Creating Service acted to get data from datasource and pass them when called

---
+++ src/main/kotlin/com/example/demo1/service/BankService.kt
@@ -0,0 +1,13 @@
+package com.example.demo1.service
+
+import com.example.demo1.datasource.BankDataSource
+import com.example.demo1.model.Bank
+import org.springframework.stereotype.Service
+
+
+@Service
+class BankService(private val dataSourse: BankDataSource) {
+    fun getBanks(): Collection<Bank> = dataSourse.retrieveBanks()
+
+    fun getBank(accountNumber: String): Bank = dataSourse.retrieveBank(accountNumber)
+}
