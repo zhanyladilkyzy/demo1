98b0901859631f33666a3c8b0e9acdc601f5c533
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-06-22 15:51:44 +0600

    add @Bean restTemplate

--- src/main/kotlin/com/example/demo1/Demo1Application.kt
+++ src/main/kotlin/com/example/demo1/Demo1Application.kt
@@ -2,9 +2,15 @@ package com.example.demo1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class Demo1Application
+class Demo1Application{
+	@Bean
+	fun restTemplate(builder: RestTemplateBuilder) : RestTemplate = builder.build()
+}
 
 fun main(args: Array<String>) {
 	runApplication<Demo1Application>(*args)
