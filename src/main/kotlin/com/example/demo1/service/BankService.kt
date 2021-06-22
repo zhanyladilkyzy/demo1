cb8ca074834e35dc5dab1958fa5f5aa855aca492
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-06-22 15:51:20 +0600

    add @Qualifier annotation for determining, which realization should we use

--- src/main/kotlin/com/example/demo1/service/BankService.kt
+++ src/main/kotlin/com/example/demo1/service/BankService.kt
@@ -2,11 +2,12 @@ package com.example.demo1.service
 
 import com.example.demo1.datasource.BankDataSource
 import com.example.demo1.model.Bank
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 
 @Service
-class BankService(private val dataSourse: BankDataSource) {
+class BankService(@Qualifier("network")private val dataSourse: BankDataSource) {
     fun getBanks(): Collection<Bank> = dataSourse.retrieveBanks()
 
     fun getBank(accountNumber: String): Bank = dataSourse.retrieveBank(accountNumber)
