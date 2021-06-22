71e394b27c551ca1a0499f43e6584d08796a1bd8
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-06-22 15:49:57 +0600

    add JsonProperty

--- src/main/kotlin/com/example/demo1/model/Bank.kt
+++ src/main/kotlin/com/example/demo1/model/Bank.kt
@@ -1,8 +1,15 @@
 package com.example.demo1.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Bank(
+    @JsonProperty("account_number")
     val accountNumber: String,
+
+    @JsonProperty("trust")
     val trust: Double,
+
+    @JsonProperty("default_transaction_fee")
     val transactionFee: Int
 )
 
